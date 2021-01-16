9900d616daa5c39b39655c9d41f1735eae4bda08
fabiomsrs <fabiomsrs@gmail.com>
2021-01-16 11:12:33 -0300

    Update cielo.ts

--- src/common/constants/cielo.ts
+++ src/common/constants/cielo.ts
@@ -1,8 +1,8 @@
 import { Cielo, CieloConstructor } from 'cielo';
 
 const cieloParams: CieloConstructor = {
-    merchantId: '86415780-89a9-4ff8-8c51-5502010788d8',
-    merchantKey: 'GAVGANMCMUZFDBALWZJDFYKXFELGWELISZURLZRZ',
+    merchantId: '',
+    merchantKey: '',
     sandbox: true, // Opcional - Ambiente de Testes
     debug: true, // Opcional - Exibe os dados enviados na requisição para a Cielo
 };
